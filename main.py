6866907370954699f18065d02a0862f8e02cdbc7
Daniel <37455228+danieljcoh@users.noreply.github.com>
2023-02-27 23:44:41 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+from turtle import Turtle, Screen
+import turtle
+
+kleon = Turtle()
+kleon.shape("turtle")
+kleon.color("chartreuse")
+
+kleon.forward(100)
+kleon.right(90)
+kleon.forward(100)
+kleon.right(90)
+kleon.forward(100)
+kleon.right(90)
+kleon.forward(100)
+kleon.right(90)
+
+screen = Screen()
+screen.exitonclick()
+#
+#
+# counter = 0
+# while counter != 4:
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.right(90)
+#     counter += 1
+
+
+# timmy = Turtle()
+#
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("chartreuse")
+# timmy.forward(100)
+#
+# myScreen = Screen()
+# print(myScreen.canvheight)
+# myScreen.exitonclick()
